ef2efc5757a6be9e1da8d0e12d0725af83dc70ef
caio felipe <fcaio8966@gmail.com>
2025-05-26 22:05:25 -0300

    att

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+var form =document.getElementById("form-cursos")
+
+form.addEventListener("Submit",function(event){
+    event.preventDefault();
+
+    var nomeCurso = document.getElementById("nomeCurso")
+    var cagarCurso = document.getElementById("carga-horaria-curso")
+
+    var meusCursos = []
+
+} )
